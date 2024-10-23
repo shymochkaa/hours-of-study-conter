24daa49e36ab59aeeedc5d2bea2c0fbab4a64cc8
Hanna Shymanovych <shimsnov111@gmail.com>
2024-10-23 15:57:33 +0200

    Refactor event handling in study tracker

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let clearBtn = document.getElementById("clear-btn")
+let incrementBtn = document.getElementById("increment-btn")
+let saveBtn = document.getElementById("save-btn")
 let count = 0
+let addHyphen = false
 
 
+incrementBtn.addEventListener("click", increment)
 
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+saveBtn.addEventListener("click", save)
+
 function save() {
-    let countStr = count + " - "
+
+    if (addHyphen == true){
+        countStr =  " - " + count 
+    } else {
+        countStr =  " " + count 
+        addHyphen = true
+    }
+    
     saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
 }
-
 
 clearBtn.addEventListener("click", deleteAll);
 
@@ -25,5 +37,6 @@ function deleteAll() {
     saveEl.textContent = "Previous entries: "
     countEl.textContent = 0
     count = 0
+    addHyphen = false
     
 }
